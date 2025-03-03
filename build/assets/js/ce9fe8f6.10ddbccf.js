f05c183c2a027944014459620754bd59d0133f31
Junseok Yang <junseokyang@Junseoks-MacBook-Pro.local>
2025-03-04 00:44:22 +0900

    build update

--- build/assets/js/ce9fe8f6.10ddbccf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfootgrammer=self.webpackChunkfootgrammer||[]).push([[7889],{4135:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/footgrammer/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
