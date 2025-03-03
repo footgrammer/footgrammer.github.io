1fdcbbd0c1752e7c216abe162bee5ee220d5e886
Junseok Yang <junseokyang@Junseoks-MacBook-Pro.local>
2025-03-04 00:42:38 +0900

    [UPDATE] change baseURL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://footgrammer.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/footgrammer/',
+  baseUrl: '/',
   trailingSlash: true,
 
   // GitHub pages deployment config.
